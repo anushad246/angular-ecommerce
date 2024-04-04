ee2a2ff490735acc9cf0cac87f9e27638dcae806
AzureAD\AnushaDammalapati <anushadammalapati@aimlytics.ai>
2024-04-04 15:48:07 +0530

    data bing to table and added router-outlet

--- src/app/main/modules/crud/crud-routing.module.ts
+++ src/app/main/modules/crud/crud-routing.module.ts
@@ -1,26 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CrudComponent } from './crud.component';
+import { EmpDetailsComponent } from './emp-details/emp-details.component';
+import { AddEditComponent } from './add-edit/add-edit.component';
 
 const CrudRoutes: Routes = [
   {
     path: '',
     component: CrudComponent,
 
-    // children: [
-    //   { path: '', redirectTo: 'product-details', pathMatch: 'full' },
-    //   {
-    //     path: 'add-edit',
-    //     loadChildren: () =>
-    //       import('./').then(
-    //         (m) => m.AddProductsModule
-    //       ),
-    //   },
-    // ],
+    children: [
+      { path: '', redirectTo: 'emp-details', pathMatch: 'full' },
+      { path: 'emp-details', component: EmpDetailsComponent },
+      { path: 'add-update', component: AddEditComponent }
+    ],
   },
 ];
-
-
 
 
 @NgModule({
